f0019a21d3bfb6f882daafac24589c61c9d38b0a
Oleg Kova <okovalchuk722@gmail.com>
2025-01-07 00:22:27 +0300

    stage 01 - 1

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import {Accordion} from "./components/accordion/Accordion";
+import { Rating } from './components/rating/Rating';
+
+type PageTitlePropsType = {
+    title: string
+}
+
+function App() {
+  return (
+    <div className="App">
+      <PageTitle title={" This is APP component "}/>
+      <Rating value={0}/>
+      <Accordion titleValue={"Menu"} collapsed={true}/>
+      <Accordion titleValue={"User"} collapsed={false}/>
+      <Rating value={1}/>
+      <Rating value={2}/>
+      <Rating value={3}/>
+      <Rating value={4}/>
+      <Rating value={5}/>
+    </div>
+  );
+}
+
+
+function PageTitle(props: PageTitlePropsType) {
+  return <h1>{props.title}</h1>
+}
+
+export default App;
